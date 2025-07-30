94eae60e3a5b0fef24818f7ad7abe4d2ad69cd13
Sahil Sharma <officialsahil548k@gmail.com>
2025-07-30 08:38:04 +0530

    new pages added

--- index.js
+++ index.js
@@ -12,6 +12,26 @@ app.get('/',(req,res)=>{
     res.render('home.ejs')
 })
 
+app.get('/login',(req,res)=>{
+  res.render('login.ejs')
+})
+
+app.get('/signup',(req,res)=>{
+  res.render('signup.ejs')
+})
+
+app.get('/patient',(req,res)=>{
+  res.render('patients.ejs')
+})
+
+app.get('/doctor',(req,res)=>{
+  res.render('doctor.ejs')
+})
+
+app.get('/appointement',(req,res)=>{
+  res.render('appointment.ejs')
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
